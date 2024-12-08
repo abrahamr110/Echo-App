0b8e5317e63a29e2bcaf353f6df73dc678ec7baf
Abraham Rodriguez <arm.developer00@gmail.com>
2024-12-08 13:17:14 +0100

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,22 +4,25 @@ import Image from "next/image";
 
 export default function Login() {
     return (
-        <div className="bg-gradient-to-b from-[#1976D2] to-[#023047] h-full w-full">
-            <div className="flex flex-row flex-grow justify-center items-center">
-                <div>
-                    <Image
-                        src="/Echo.png"
-                        alt="Logo"
-                        width={1000}
-                        height={1000}
-                    />
-                </div>
+        <div className="bg-gradient-to-b from-[#1976D2] to-[#023047] h-screen w-screen flex flex-col">
+            <div className="flex-grow flex items-center justify-center">
+                <div className="flex flex-col md:flex-row items-center gap-4">
+                    <div className="md:w-1/2 md:h-1/2">
+                        <Image
+                            src="/Echo.png"
+                            alt="Logo"
+                            width={1000}
+                            height={1000}
+                        />
+                    </div>
 
-                <Form />
+                    <Form />
+                </div>
             </div>
-            <div className="flex flex-row justify-center gap-4">
+
+            <footer className="flex flex-row justify-center gap-4 py-4 px-2 w-full shadow-md">
                 <Footer />
-            </div>
+            </footer>
         </div>
     );
 }
