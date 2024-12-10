7e2b7dc6dfa3d8893d833a4230d751e70e23db6b
Abraham Rodriguez <arm.developer00@gmail.com>
2024-12-10 17:56:48 +0100

    página home casi finalizada

--- app/components/content.jsx
+++ app/components/content.jsx
@@ -1,1 +1,77 @@
-export const Content = () => {};
+import Image from "next/image";
+
+const tweets = [
+    {
+        avatar: "/avatar.png",
+        name: "Abraham",
+        username: "@abrahamcastillo",
+        image: "/Echo.png",
+        description:
+            "El director médico de Inglaterra dice que el Reino Unido se encuentra en el momento más peligroso de la pandemia",
+    },
+    {
+        avatar: "/avatar.png",
+        name: "Abraham",
+        username: "@abrahamcastillo",
+        image: "/Echo.png",
+        description:
+            "El director médico de Inglaterra dice que el Reino Unido se encuentra en el momento más peligroso de la pandemia",
+    },
+];
+
+export const Content = () => {
+    return (
+        <div className="flex flex-col w-full h-full p-4">
+            <p>Inicio</p>
+            <div className="flex flex-row">
+                <Image
+                    src="/avatar.png"
+                    alt="Imagen"
+                    width={40}
+                    height={40}
+                    className="rounded-full"
+                />
+                <p className="text-black text-xl">¿Qué está pasando?</p>
+                <button className="text-blue-500 border border-blue-500 rounded-3xl text-sm bg-white px-4 py-2">
+                    Echo
+                </button>
+            </div>
+            {tweets.map((tweet, index) => {
+                return (
+                    <div key={index} className="flex flex-row">
+                        <Image
+                            src={tweet.avatar}
+                            alt="Imagen"
+                            width={40}
+                            height={40}
+                            className="rounded-full"
+                        />
+                        <div className="flex flex-col">
+                            <div className="flex flex-row items-center gap-4">
+                                <p className="text-[#7A7C7F] text-sm">
+                                    {tweet.name}
+                                </p>
+                                <p className="text-[#7A7C7F] text-sm">
+                                    {tweet.username}
+                                </p>
+                            </div>
+                            <p className="text-sm text-black">
+                                {tweet.description}
+                            </p>
+                            <Image
+                                src={tweet.image}
+                                alt="Imagen"
+                                width={40}
+                                height={40}
+                                className="rounded-full"
+                            />
+                            <button className="text-blue-500 bg-black">
+                                Mostrar hilo
+                            </button>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,19 @@
 import { SideBar } from "./components/sideBar";
 import { RightBar } from "./components/rightBar";
+import { Content } from "./components/content";
 
 export default function Home() {
     return (
         <div className="flex flex-col lg:flex-row">
-            <div className="w-full lg:w-1/4 h-auto  rounded-lg shadow-md">
+            <div className="w-full lg:w-1/4 h-auto rounded-lg shadow-md">
                 <SideBar />
             </div>
 
-            <div className="w-full lg:w-2/4 h-auto lg:h-screen  rounded-lg shadow-md flex items-center justify-center">
-                <p className="text-gray-700 text-xl text-center">
-                    Componente Central (Más espacio)
-                </p>
+            <div className="w-full lg:w-2/4 h-auto lg:h-screen rounded-lg shadow-md flex items-center justify-center">
+                <Content />
             </div>
 
-            <div className="w-full lg:w-1/4 h-auto lg:h-screen bg-gray-100  rounded-lg shadow-md">
+            <div className="w-full lg:w-1/4 h-auto lg:h-screen bg-gray-100 rounded-lg shadow-md">
                 <RightBar />
             </div>
         </div>
